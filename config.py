7556a4b3cdf1a0b315f08a0ceb3dc49ecef80701
Cheng Zhang <51064582+cc0426@users.noreply.github.com>
2026-05-06 10:17:43 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,45 +3,41 @@ import torch
 
 
 class TrainingConfig:
-    """训练配置"""
+
 
     def __init__(self):
         self.num_epochs = 2000
         self.batch_size = 128
         self.learning_rate = 0.001
         self.weight_decay = 1e-3
-        self.save_interval = 5  # 每多少epoch保存一次
+        self.save_interval = 5 
         self.save_dir = "./checkpoints/stage1"
 
-        # 优化器
+
         self.optimizer_type = 'adam'  # 'adam', 'adamw', 'sgd'
 
-        # 梯度裁剪
+
         self.grad_clip = True
         self.grad_clip_value = 1.0
 
-        # 提前停止
         self.early_stopping_threshold = 1e-4
 
 
-        # 训练控制
-        self.max_batches_per_epoch = None  # 设为None表示使用所有batch
-        self.resume_checkpoint = "/home/zhangcheng/Soil_Moisture/CML_FD/checkpoints/stage1/stage1_best_model.pth"  # 恢复训练的检查点路径
+        self.max_batches_per_epoch = None
+        self.resume_checkpoint = "/home/zhangcheng/Soil_Moisture/CML_FD/checkpoints/stage1/stage1_best_model.pth" 
 
-        # 日志和监控
+
         self.use_wandb = False
         self.wandb_project = "soil_moisture_stage1"
 
-
-        # 设备
         self.device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
 
 
 class DataConfig:
-    """数据配置"""
+ 
 
     def __init__(self):
-        # 数据路径
+    
         self.data_root = 'data/'
         self.dynamic_data_path = './dataset/forcing.npy'
         self.static_data_path = './dataset/static_variables.npy'
@@ -54,7 +50,7 @@ class DataConfig:
 
         self.data_start_date = '2010-01-01'
         self.data_end_date = '2018-12-31'
-        # 数据参数
+ 
         self.train_start_date = '2010-01-01'
         self.train_end_date = '2016-12-31'
         self.val_start_date = '2017-01-01'
@@ -63,12 +59,12 @@ class DataConfig:
         self.test_end_date = '2018-12-31'
 
 
-# 配置示例
+
 class ModelConfig:
     def __init__(self):
         self.input_dim = 12
         self.shared_dim = 128
-        # self.dynamic_channels = 7  # 强迫数据特征数
+        # self.dynamic_channels = 7 
         # self.static_channels = 5
         # self.lstm_hidden_dim = 512
         # self.lstm_num_layers = 2
@@ -81,7 +77,7 @@ class ModelConfig:
 
 
 
-# # 产品配置
+
 PRODUCT_CONFIGS = {
     'era5': {
         'resolution': (160, 200),
